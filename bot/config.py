97ea170d6614cd51fd92979a8da9b64e10833609
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2024-01-26 16:19:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -10,7 +10,7 @@ try:
     API_HASH = "a40f901dbfdca8909907caabf840f606"
     BOT_TOKEN = config("BOT_TOKEN", "6828621030:AAG85sLPvi42Up0myh5hpnbFB7A_L5l50gQ")
     ffmpegcode = ["-c:v libx265 -x265-params 'crf=24:bframes=10:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1:profile=main10:level=3.1' -pix_fmt yuv420p10le -preset veryslow -s 1280x720 -metadata 'title=Encoded By D2K' -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -threads 1"]
-    THUMBNAIL = "https://telegra.ph/file/711777fbb7317a73c211a.jpg"
+    THUMBNAIL = "https://telegra.ph/file/27466c8e687e13be6a1e3.jpg"
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
